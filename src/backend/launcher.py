4c8ff79261de6dc0abc0e2184b6772b2484180cc
Adrian Burgos <awb8593@rit.edu>
2024-01-20 15:00:19 -0500

    Created launcher.py to grab all games from the game directory, turn them into game objects, and store them

--- src/backend/launcher.py
+++ src/backend/launcher.py
@@ -1,1 +1,11 @@
 import game
+import os
+
+games = {}
+games_directory = ""; # TODO need this
+file_list = os.listdir(games_directory) # create a list of each file (name) in the directory
+
+for file in file_list:
+    file_path = os.path.join(games_directory, file)
+    game_inst = game.Game("", file_path, "")
+    games[file] = game_inst
