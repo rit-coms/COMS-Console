fd11cb5528ba75507575d0080f0506a55240e0f5
Adrian Burgos <awb8593@rit.edu>
2024-01-20 13:49:24 -0500

    Added game class

---
+++ game.py
@@ -0,0 +1,8 @@
+# class to store each game's info
+
+class Game:
+    def __init__(self, name, file_location, summary):
+        self.name = name
+        self.file_location = file_location,
+        self.summary = summary
+    
