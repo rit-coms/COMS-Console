3d3f81325167a7b93108047a960214959b40815d
Adrian Burgos <awb8593@rit.edu>
2024-02-29 22:04:08 -0500

    Added json storing to backend - Not complete, need to troubleshoot

--- src/backend/launcher.py
+++ src/backend/launcher.py
@@ -1,11 +1,28 @@
+import json
 import game
 import os
 
-games = {}  # the list of each Game - frontend devs use this to access all Game objects
-games_directory = "/home/pi/ConsoleGames"
-file_list = os.listdir(games_directory) # create a list of each file (name) in the directory
+# games_directory = "/home/pi/ConsoleGames"
+games_directory = r"C:\Users\Adrian\Documents\Games"
 
-for file in file_list:
-    file_path = os.path.join(games_directory, file)
-    game_inst = game.Game(file, file_path, '', '', 0, False, [])  # create an instance of a Game object
-    games[file] = game_inst
+# adds new games in the games directory to the game_data json file
+def scan():
+    games = {}
+    file_list = os.listdir(games_directory) # create a list of each file (name) in the directory
+    for file in file_list:
+        file_path = os.path.join(games_directory, file)
+        game_inst = game.Game(file, file_path, '', '', 0, False, [])  # create an instance of a Game object
+        games[file] = game_inst
+    with open(r"C:\Users\Adrian\Desktop", "r") as current:
+        old_data = json.load(current)
+    
+    # pick out the new games
+    for g in games:
+        for old in old_data:
+            if(g.file_path == old.file_path):
+                games.popitem(g)
+
+    # update the json file
+    with open(r"C:\Users\Adrian\Desktop", "w") as p:
+        json.dump(games, p)
+
