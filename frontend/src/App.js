060ead3feba2eedb36419b0ed3223851e518fcda
Zoe <zmb6893@g.rit.edu>
2024-02-29 21:48:05 -0500

    Added in a keyboard and search mechanism

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import GameGallery from './components/GameGallery';
+import GameSearchOverlay from './components/GameSearchOverlay';
 
 function App() {
+  const games = [
+		{
+			name: "QuackAttack",
+			author: "Zoe"
+		},
+		{
+			name: "BossDuck",
+			author: "Jeff"
+		},
+		{
+			name: "QuackQuackGo",
+			author: "Jeff"
+		},
+		{
+			name: "DuckRecker",
+			author: "Adrian"
+		},
+	]
   return (
     <div>
       <h1>QuackBox React Boiler plate code</h1>
+      <GameSearchOverlay games={games}></GameSearchOverlay>
       <GameGallery></GameGallery>
     </div>
   );
--- frontend/src/components/GameSearchOverlay.js
+++ frontend/src/components/GameSearchOverlay.js
@@ -1,7 +1,56 @@
-export default function GameSearchOverlay () {
+import { useState } from 'react';
+import Keyboard from 'react-simple-keyboard';
+import 'react-simple-keyboard/build/css/index.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+function GameSearchOverlay ({games}) {
+	let [search, setSearch] = useState("")
+	let [searchResults, setSearchResults] = useState([]);
+
+	const onChange = (input) => {
+		search = setSearch(input);
+		console.log("Search changed", search);
+	  }
+	
+	const onKeyPress = (button) => {
+		console.log("Button pressed", button);
+		// When enter is clicked, search for the game
+		if (button == "{enter}") {
+			console.log("ENTER")
+			searchGame(search);
+		}
+	}
+
+	const searchGame = (searchTerm) => {
+		let searchResults = []
+		games.forEach(game => {
+			// Add the game to an array of search results if the search term is included in the name or author
+			if (game.name.toLowerCase().includes(searchTerm) || game.author.toLowerCase().includes(searchTerm)) {
+				searchResults.push(game);
+			}
+		});
+
+		console.log(searchResults);
+		setSearchResults(searchResults);
+
+		return searchResults;
+	}
+	
 	return (
 		<div>
-			This will be a slightly transparent overlay that has a keyboard to search for a specific game.
+
+			<h1 style={{
+				"border-radius": "15px", 
+				"width": "100%",
+				"background-color": "#f2f2f2",
+				"textAlign": "center"
+			}}>{search ? search : "Enter in a game or author..."}</h1>
+			<Keyboard
+				onChange={onChange}
+				onKeyPress={onKeyPress}
+			/>
 		</div>
 	)
 }
+
+export default GameSearchOverlay;
