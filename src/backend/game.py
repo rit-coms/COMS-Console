710ad978763984dd572f0593bf5ab63598fb1364
Adrian Burgos <awb8593@rit.edu>
2024-02-29 19:35:19 -0500

    Added additional attributes to Game and removed loop that launched all games

--- src/backend/game.py
+++ src/backend/game.py
@@ -2,10 +2,15 @@
 import subprocess
 
 class Game:
-    def __init__(self, name, file_path, summary):
-        self.name = name
-        self.file_path = file_path,
+    def __init__(self, title, file_path, author, summary, release_date, is_multiplayer, genres):
+        self.title = title
+        self.file_path = file_path
+        self.author = author
         self.summary = summary
+        self.release_date = release_date
+        self.is_multiplayer = is_multiplayer
+        self.genres = genres
     
+    # launch the game
     def launch(self):
         subprocess.Popen(self.file_path)
--- src/backend/launcher.py
+++ src/backend/launcher.py
@@ -1,15 +1,12 @@
 import game
 import os
 
-games = {}
-games_directory = "/home/pi/ConsoleGames";
+games = {}  # the list of each Game - frontend devs use this to access all Game objects
+# games_directory = "/home/pi/ConsoleGames"
+games_directory = r"C:\Users\Adrian\Documents\Games"
 file_list = os.listdir(games_directory) # create a list of each file (name) in the directory
 
 for file in file_list:
     file_path = os.path.join(games_directory, file)
-    game_inst = game.Game("", file_path, "")  # create an instance of a Game object
+    game_inst = game.Game(file, file_path, '', '', 0, False, [])  # create an instance of a Game object
     games[file] = game_inst
-
-print(games)
-for game_name, game_inst in games.items():
-    game_inst.launch()
