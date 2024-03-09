d1a19840802437411ef5ea6792b1dffb2e07d8d1
m-dorsey <dorseymakenzie@gmail.com>
2024-03-09 11:39:30 -0500

    implemented the home screen

--- frontend/src/components/GameThumbnail.js
+++ frontend/src/components/GameThumbnail.js
@@ -1,6 +1,9 @@
+
+import '../styles/GameGallery.css'
+
 export default function GameThumbnail() {
 	return (
-		<div>
+		<div className='card'>
 			<h2>Add GameThumbnail template code here!</h2>
 		</div>
 	)
