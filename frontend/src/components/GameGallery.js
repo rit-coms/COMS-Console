d5fe9b0bf73237e52a4756aa02925b2df93799b1
KasimRIT <klo7619@rit.edu>
2024-03-13 19:36:37 -0400

    Initial implementation of see-all

--- frontend/src/components/GameGallery.js
+++ frontend/src/components/GameGallery.js
@@ -1,27 +1,31 @@
+import React, { useState } from 'react';
 import GameThumbnail from "./GameThumbnail";
-import '../styles/GameGallery.css'
-import {NavLink} from 'react-router-dom'
+import '../styles/GameGallery.css';
+import { NavLink } from 'react-router-dom';
 
-const games = ["1", "2", "3", "4", "5", "6", "7", "8"]
+const allGames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"];
 
 export default function GameGallery() {
-	
-	return (
-		<div className="game-gallery">
-			
-			<NavLink to='/see-all'>
-				<div className="card" style={{'backgroundColor':'#e6e6e6'}}>
-					See All
-				</div>
-			</NavLink>
-			
-			{/* For every game, add thumbnail */}
-			{
-				games.map(game => (
-					<GameThumbnail key={game}></GameThumbnail>
-				))
-			}
-			
-		</div>
-	)
+    const [showFullGallery, setShowFullGallery] = useState(false);
+
+    const handleSeeAllClick = () => {
+        setShowFullGallery(true);
+    };
+
+    return (
+        <div className="game-gallery">
+            {!showFullGallery && (
+                <NavLink to='/see-all' onClick={handleSeeAllClick}>
+                    <div className="card" style={{ 'backgroundColor': '#e6e6e6' }}>
+                        See All
+                    </div>
+                </NavLink>
+            )}
+
+            {showFullGallery
+                ? allGames.map((game) => <GameThumbnail key={game}></GameThumbnail>)
+                : games.slice(0, 6).map((game) => <GameThumbnail key={game}></GameThumbnail>)
+            }
+        </div>
+    );
 }
