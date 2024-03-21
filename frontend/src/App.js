bf263780094bcbff203fc3310b0e6fbfdd7b30f1
Zoe <zmb6893@g.rit.edu>
2024-03-21 10:26:51 -0400

    Added search to modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,21 @@
 import './App.css';
-import GameGallery from './components/GameGallery';
-import GameSearchOverlay from './components/GameSearchOverlay';
 import GameSearchOverlay from './components/GameSearchOverlay';
 import HomePage from './pages/HomePage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-  const games = [
-		{
-			name: "QuackAttack",
-			author: "Zoe"
-		},
-		{
-			name: "BossDuck",
-			author: "Jeff"
-		},
-		{
-			name: "QuackQuackGo",
-			author: "Jeff"
-		},
-		{
-			name: "DuckRecker",
-			author: "Adrian"
-		},
-	]
-  return (
-    <BrowserRouter>
-      <h1>QuackBox React Boiler plate code</h1>
-      <GameSearchOverlay games={games}></GameSearchOverlay>
-      <GameGallery></GameGallery>
-    </div>
-      
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/search' element={<GameSearchOverlay />} />
-      </Routes>
-    
-      </BrowserRouter>
-  );
+
+	return (
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+					<Route path='/search' element={<GameSearchOverlay />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+		
+	);
 }
 
 export default App;
