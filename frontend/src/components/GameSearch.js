943215f53e34e61bcb99f17f6ba72c20e76d7789
m-dorsey <dorseymakenzie@gmail.com>
2024-04-13 23:47:52 -0400

    edited search modal

--- frontend/src/components/GameSearch.js
+++ frontend/src/components/GameSearch.js
@@ -8,9 +8,9 @@ function GameSearch() {
     let { keyboardClick } = useContext(SearchContext)
 
     const keyboard = [
-        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'del'],
+        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
         ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-        ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+        ['del', 'z', 'x', 'c', 'v', 'b', 'n', 'm'],
         ['space']
     ]
 
