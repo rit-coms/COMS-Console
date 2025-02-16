050074241a6cb985d0a7071311c6f0c486c83b6a
saoski <saoski88@gmail.com>
2025-02-16 16:41:41 -0500

    Fixed file formatting

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,21 +19,22 @@ fn running_on_rasp_pi() -> bool {
 
 fn main() {
     tauri::Builder::default()
-    .plugin(tauri_plugin_autostart::init(
-        MacosLauncher::LaunchAgent,
-        Some(vec![""]),
-    ))
-    .setup(|app| {
-        app.manage(Mutex::new(AppState::default()));
-        if running_on_rasp_pi() { // Only enable on raspberry pi
-            app.autolaunch().enable()?;
-        }
-        Ok(())
-    })
-    .invoke_handler(tauri::generate_handler![get_game_info, play_game])
-    .on_page_load(|window, _| {
-        window.show().expect("Failed to show window");
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![""]),
+        ))
+        .setup(|app| {
+            app.manage(Mutex::new(AppState::default()));
+            if running_on_rasp_pi() {
+                // Only enable on raspberry pi
+                app.autolaunch().enable()?;
+            }
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![get_game_info, play_game])
+        .on_page_load(|window, _| {
+            window.show().expect("Failed to show window");
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
