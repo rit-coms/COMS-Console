9a1f84440d9833cc2f9028930d16411f2a00ed25
saoski <saoski88@gmail.com>
2025-02-20 23:11:08 -0500

    Basic functional web server

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,15 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use frontend_api::games::{get_game_info, AppState, play_game};
+use frontend_api::games::{get_game_info, play_game, AppState};
+use game_dev_api::hello_axum;
 use tauri::Manager;
 use tauri_plugin_autostart::{MacosLauncher, ManagerExt};
 
 use std::sync::Mutex;
 
 mod frontend_api;
+mod game_dev_api;
 
 fn main() {
     tauri::Builder::default()
@@ -18,6 +20,7 @@ fn main() {
         .setup(|app| {
             app.manage(Mutex::new(AppState::default()));
             app.autolaunch().enable()?;
+            tauri::async_runtime::spawn(hello_axum::setup_routes());
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![get_game_info, play_game])
