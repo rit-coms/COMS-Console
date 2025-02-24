0771028385c598dc54de96842470d680092d7e2f
saoski <saoski88@gmail.com>
2025-02-24 10:42:11 -0500

    Remove example route

--- src-tauri/src/game_dev_api/api.rs
+++ src-tauri/src/game_dev_api/api.rs
@@ -1,4 +1,9 @@
-use axum::{extract::Query, response::IntoResponse, routing::{get, post}, Json, Router};
+use axum::{
+    extract::Query,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
 use serde::Deserialize;
 
 const VERSION: u8 = 1;
@@ -12,21 +17,12 @@ struct LeaderboardEntry {
 #[derive(Deserialize)]
 enum LeaderboardScope {
     User,
-    Global
+    Global,
 }
 
 #[derive(Deserialize)]
 struct LeaderboardGetParams {
-    scope: LeaderboardScope
-}
-
-/// Example get request handler
-async fn hello_world_handler() -> impl IntoResponse {
-    let json_response = serde_json::json!({
-        "message":"Hello world!"
-    });
-
-    Json(json_response)
+    scope: LeaderboardScope,
 }
 
 async fn set_leaderboard(Json(payload): Json<LeaderboardEntry>) {
@@ -35,7 +31,10 @@ async fn set_leaderboard(Json(payload): Json<LeaderboardEntry>) {
     let user_id = 0;
 
     // Save entry to database
-    println!("Saved to database: {{user_id:{user_id:?}, gameid:{game_id:?}, tag:{0:?}, value:{1:?}}}", payload.tag, payload.value)
+    println!(
+        "Saved to database: {{user_id:{user_id:?}, gameid:{game_id:?}, tag:{0:?}, value:{1:?}}}",
+        payload.tag, payload.value
+    )
 }
 
 async fn get_leaderboard(params: Query<LeaderboardGetParams>) -> impl IntoResponse {
@@ -59,7 +58,7 @@ async fn get_leaderboard(params: Query<LeaderboardGetParams>) -> impl IntoRespon
                 }
             ]);
             Json(json_response)
-        },
+        }
         LeaderboardScope::Global => {
             json_response = serde_json::json!([
                 {
@@ -76,7 +75,7 @@ async fn get_leaderboard(params: Query<LeaderboardGetParams>) -> impl IntoRespon
                 }
             ]);
             Json(json_response)
-        },
+        }
     }
 }
 
@@ -89,10 +88,14 @@ async fn get_all_save_paths() -> impl IntoResponse {}
 pub async fn setup_game_dev_api() {
     let route_prefix: String = format!("/api/v{VERSION:?}");
     let app = Router::new()
-        .route(&format!("{route_prefix:?}/healthcheck"), get(hello_world_handler))
-        .route(&format!("{route_prefix:?}/leaderboard"), post(set_leaderboard).get(get_leaderboard))
-        .route(&format!("{route_prefix:?}/save-data"), post(set_save_data).get(get_save_data));
-    
+        .route(
+            &format!("{route_prefix:?}/leaderboard"),
+            post(set_leaderboard).get(get_leaderboard),
+        )
+        .route(
+            &format!("{route_prefix:?}/save-data"),
+            post(set_save_data).get(get_save_data),
+        );
 
     println!("Server started successfully!!!");
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
