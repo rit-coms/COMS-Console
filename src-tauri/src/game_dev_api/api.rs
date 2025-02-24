18990ac071246a8c0ae9c7cfe722996a02d150ec
saoski <saoski88@gmail.com>
2025-02-24 13:35:53 -0500

    Finish implementation of leaderboard and save data handlers

--- src-tauri/src/game_dev_api/api.rs
+++ src-tauri/src/game_dev_api/api.rs
@@ -15,6 +15,12 @@ struct LeaderboardEntry {
 }
 
 #[derive(Deserialize)]
+struct SaveDataEntry {
+    file_name: String,
+    data: serde_json::Value, // This data should be stored in the database as BSON data, is this the correct type?
+}
+
+#[derive(Deserialize)]
 enum LeaderboardScope {
     User,
     Global,
@@ -32,7 +38,7 @@ async fn set_leaderboard(Json(payload): Json<LeaderboardEntry>) {
 
     // Save entry to database
     println!(
-        "Saved to database: {{user_id:{user_id:?}, gameid:{game_id:?}, tag:{0:?}, value:{1:?}}}",
+        "Saved to database: {{user_id:{user_id:?}, game_id:{game_id:?}, tag:{0:?}, value:{1:?}}}",
         payload.tag, payload.value
     )
 }
@@ -41,6 +47,7 @@ async fn get_leaderboard(params: Query<LeaderboardGetParams>) -> impl IntoRespon
     let json_response: serde_json::Value;
     match params.scope {
         // TODO: query databse
+        // TODO: should we exclude game and user id from response since we are handling that instead of the game developer?
         LeaderboardScope::User => {
             // Example data with two leaderboard entries
             json_response = serde_json::json!([
@@ -79,9 +86,30 @@ async fn get_leaderboard(params: Query<LeaderboardGetParams>) -> impl IntoRespon
     }
 }
 
-async fn set_save_data() {}
+async fn set_save_data(Json(payload): Json<SaveDataEntry>) {
+    let game_id = 32;
+    let user_id = 53;
+    let bson_data = bson::to_bson(&payload.data);
 
-async fn get_save_data() -> impl IntoResponse {}
+    // Save entry to database;
+    println!(
+        "Saved to database: {{user_id:{user_id:?}, game_id:{game_id:?}, file_id{:?}}} with also some BSON data",
+        payload.file_name
+    )
+}
+
+async fn get_save_data() -> impl IntoResponse {
+    // TODO: parse BSON from database bask into json
+    let data: String = r#"{"money":32, "level":3}"#.to_string();
+    let json_response = serde_json::json!([
+        {
+            "file_name":"2_24_2025",
+            "data": data
+        }
+    ]);
+
+    Json(json_response)
+}
 
 async fn get_all_save_paths() -> impl IntoResponse {}
 
