3622d52bde2f0730ba24e65d9cbf80c45195c127
saoski <saoski88@gmail.com>
2025-03-10 22:09:16 -0400

    Add docs

--- src-tauri/src/game_dev_api/mod.rs
+++ src-tauri/src/game_dev_api/mod.rs
@@ -1,13 +1,33 @@
-use axum::{
-    routing::post,
-    Router,
-};
+use axum::{routing::post, Router};
 use handlers::{get_leaderboard, get_save_data, set_leaderboard, set_save_data, ApiState};
 
 const VERSION: u8 = 1;
 
 pub mod handlers;
 
+/// Creates an Axum router with leaderboard and save-data post and get handlers.
+///
+/// # Arguments
+///
+/// * `db_name` - A reference to the name of the database file. If this router isn't
+/// for testing, make sure this matches the name of the .db file in the DATABASE_URL
+/// envirenment variable
+///
+/// # Returns
+///
+/// * `Router` - An axum router with the route handlers stated above.
+///
+/// # Example
+///
+/// ```rust
+/// let app = create_router("local");
+///
+/// let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
+///     .await
+///     .unwrap();
+///     
+/// axum::serve(listener, app).await.unwrap();
+/// ```
 pub fn create_router(db_name: &str) -> Router {
     let route_prefix: String = format!("/api/v{}", VERSION.to_string());
     let api_state = ApiState {
@@ -27,6 +47,8 @@ pub fn create_router(db_name: &str) -> Router {
         .with_state(api_state)
 }
 
+/// This function should be called in tauri builder to setup the http API for game
+/// developers to read and write game data.
 pub async fn setup_game_dev_api(db_name: &str) {
     let app = create_router(db_name);
 
