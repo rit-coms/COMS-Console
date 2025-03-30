0077144ccdfa239d8499fd8f87accba954815bd5
saoski <saoski88@gmail.com>
2025-03-30 15:34:38 -0400

    Add player slots

--- src-tauri/src/game_dev_api/handlers.rs
+++ src-tauri/src/game_dev_api/handlers.rs
@@ -6,7 +6,7 @@ use axum::{
     Json,
 };
 use serde::{Deserialize, Serialize};
-use serde_json::Value;
+use serde_json::{from_str, Value};
 use std::option::Option;
 
 #[derive(Clone)]
@@ -15,37 +15,34 @@ pub struct ApiState {
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct LeaderboardEntry {
+pub struct LeaderboardPost {
     pub value_name: String,
     pub value_num: f64,
+    pub player_slot: i16,
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct SaveDataEntry {
+pub struct SaveDataPost {
     pub file_name: String,
     pub data: serde_json::Value, // This data should be stored in the database as BSON data, is this the correct type?
-}
-
-#[derive(Deserialize, Serialize)]
-pub enum LeaderboardScope {
-    User,
-    Global,
+    pub player_slot: i16,
 }
 
 /// Handles HTTP post requests for the axum webserver by inserting the given entry in the
 /// SQLite database.
 pub async fn set_leaderboard(
     State(state): State<ApiState>,
-    Json(payload): Json<LeaderboardEntry>,
+    Json(payload): Json<LeaderboardPost>,
 ) -> impl IntoResponse {
     // TODO: Get game_id and user_id
-    let game_id = "0";
-    let user_id = "0";
+    println!("Setting Laaderboard data");
+    let game_id = "1";
+    let user_id = payload.player_slot.to_string();
 
     // Save entry to database
     // TODO: Can I return the query response from this function?
     db::insert_leaderboard_entry(
-        user_id,
+        &user_id,
         game_id,
         payload.value_name.as_str(),
         payload.value_num,
@@ -57,16 +54,17 @@ pub async fn set_leaderboard(
     Json(serde_json::json!({
         "value_name":payload.value_name,
         "value_num":payload.value_num,
+        "player_slot":payload.player_slot,
     }))
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct LeaderboardGetParams {
-    pub scope: Option<LeaderboardScope>,
     pub count: Option<i64>,
     pub ascending: Option<bool>,
     pub value_name: Option<String>,
     pub offset: Option<i64>,
+    pub player_slot: Option<i16>,
 }
 
 /// Handles HTTP leaderboard get requests for the axum webserver
@@ -74,8 +72,13 @@ pub async fn get_leaderboard(
     State(state): State<ApiState>,
     params: Query<LeaderboardGetParams>,
 ) -> impl IntoResponse {
-    let game_id: String = String::from("0"); // Example for now
-    let user_id: String = String::from("0");
+    let game_id: String = String::from("1"); // Example for now
+    let user_id_s: Option<String>;
+    // TODO: get associated player id and error check for invalid slots (negative or greater than the max)
+    match params.player_slot {
+        Some(slot) => user_id_s = Some(slot.to_string()),
+        None => user_id_s = None,
+    }
     let count: Option<i64>;
 
     // TODO: add error http response to handle when count > 100
@@ -88,16 +91,6 @@ pub async fn get_leaderboard(
     } else {
         count = Some(100);
     }
-
-    // Set the user_id according to the given leaderboard scope
-    // TODO: get actual user_id
-    let user_id_s: Option<String> = match &params.scope {
-        Some(scope) => match scope {
-            LeaderboardScope::User => Some(user_id),
-            LeaderboardScope::Global => None,
-        },
-        None => None, // If no scope query param given, default to global
-    };
 
     let leaderboard_entries = db::get_leaderboard(
         Some(game_id),
@@ -117,6 +110,7 @@ pub async fn get_leaderboard(
         json_response.push(serde_json::json!({
             "value_name": entry.value_name,
             "value_num": entry.value_num,
+            "player_slot": str::parse::<i16>(&entry.user_id).unwrap(),
         }));
     }
 
@@ -126,15 +120,15 @@ pub async fn get_leaderboard(
 // Handles save-data HTTP post requests for the axum webserver
 pub async fn set_save_data(
     State(state): State<ApiState>,
-    Json(payload): Json<SaveDataEntry>,
+    Json(payload): Json<SaveDataPost>,
 ) -> impl IntoResponse {
     let game_id = "0";
-    let user_id = "0";
+    let user_id = payload.player_slot.to_string();
 
     // Save entry to database;
     // TODO: more elegant error handling for converting json data to vec of bytes
     db::set_save(
-        user_id,
+        &user_id,
         game_id,
         payload.file_name.as_str(),
         &serde_json::to_vec(&payload.data).unwrap(),
@@ -144,7 +138,8 @@ pub async fn set_save_data(
 
     Json(serde_json::json!({
         "file_name": payload.file_name,
-        "data": payload.data
+        "data": payload.data,
+        "player_slot": payload.player_slot,
     }))
 }
 
@@ -152,6 +147,7 @@ pub async fn set_save_data(
 pub struct SaveDataGetParams {
     pub file_name: Option<String>,
     pub regex: Option<String>,
+    pub player_slot: Option<i16>,
 }
 
 /// Handles save-data HTTP get requests for the axum webserver.
@@ -163,11 +159,14 @@ pub async fn get_save_data(
 ) -> impl IntoResponse {
     println!("Getting save data!");
     let game_id: String = String::from("0"); // Example for now
-    let user_id: String = String::from("0");
+    let user_id_s: Option<String> = match params.player_slot {
+        Some(slot) => Some(slot.to_string()),
+        None => None,
+    };
 
     let save_data_entries = db::get_save_data(
         &Some(game_id),
-        &Some(user_id),
+        &user_id_s,
         &params.file_name,
         &params.regex,
         &state.db_name,
@@ -179,11 +178,12 @@ pub async fn get_save_data(
             let mut json_response = Vec::new();
 
             // TODO: add time_stamp
-            // TODO: parse binary data into json
+            // TODO: fix player slot to not use hardcoded values
             for entry in save_data {
                 json_response.push(serde_json::json!({
                             "data":serde_json::from_slice::<Value>(&entry.data).expect("Failed to deserialize BSON data"),
-                            "file_name": entry.file_name
+                            "file_name": entry.file_name,
+                            "player_slot": str::parse::<i16>(&entry.user_id).unwrap(),
                         }));
             }
             return Json(json_response).into_response();
