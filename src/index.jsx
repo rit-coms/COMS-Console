2469644ffcdeb5b9042d383a1c55a2122ccce8fa
Makenzie Dorsey <dorseymakenzie@gmail.com>
2025-04-05 23:42:42 -0500

    remove jest and web files

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,6 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { appWindow } from "@tauri-apps/api/window";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 appWindow.setFullscreen(true);
@@ -18,8 +17,3 @@ if (rootElement) {
 } else {
   console.error("Root element not found");
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
