8a186822dfa7a813e239014b250c5eeeb0bacff8
saoski <saoski88@gmail.com>
2025-04-25 22:29:53 -0400

    Fix leaderboard display I APOLOGISE FOR REFORMATTING

--- src/components/GameInfoModal.jsx
+++ src/components/GameInfoModal.jsx
@@ -1,201 +1,180 @@
 import React, { useState, useEffect } from "react";
-import { Link, Modal, Pill, Tab, Table, TableData, TableRow, Tabs, Text } from "quackbox-design-system";
+import {
+  Link,
+  Modal,
+  Pill,
+  Tab,
+  Table,
+  TableData,
+  TableRow,
+  Tabs,
+  Text,
+} from "quackbox-design-system";
 import "../styles/GameInfoModal.css";
 import { invoke } from "@tauri-apps/api/tauri";
 import { usePageContext } from "../context/contexts";
 
-import placeholder from "../assets/placeholder.png"
+import placeholder from "../assets/placeholder.png";
 
-export default function GameInfoModal({ showModal, closeModal, game}) {
+export default function GameInfoModal({ showModal, closeModal, game }) {
+  if (!showModal) return null;
 
-	if (!showModal)
-        return null;
+  const { updatePage } = usePageContext();
+  const [leaderboard, setLeaderboard] = useState([]);
+  const isPlaceholder = game.title.toLowerCase().includes("coming soon");
+  const hasCoverImage = !game.coverImage?.includes("null");
 
-	const { updatePage } = usePageContext();
-	const [leaderboard, setLeaderboard] = useState([])
-	const isPlaceholder = game.title.toLowerCase().includes("coming soon");
-	const hasCoverImage = !game.coverImage?.includes("null");
+  const formatDate = (dateString) => {
+    // yyyy-mm-dd date format
+    const dateParts = dateString.split("-");
+    const formattedDateString = `${dateParts[0]}-${String(
+      dateParts[1]
+    ).padStart(2, "0")}-${String(dateParts[2]).padStart(2, "0")}`;
 
-	const formatDate = (dateString) => {
-		// yyyy-mm-dd date format
-		const dateParts = dateString.split("-");
-		const formattedDateString = `${dateParts[0]}-${String(dateParts[1]).padStart(2, "0")}-${String(dateParts[2]).padStart(2, "0")}`;
-		
-		const date = new Date(formattedDateString);
-		const options = { year: "numeric", month: "long", day: "numeric" };
-		return date.toLocaleDateString("en-US", options);
-	};
+    const date = new Date(formattedDateString);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+  };
 
-	useEffect(() => {
-		invoke("get_leaderboard_data", {gameTitle: game.title}).then(leaderboard => {
-			console.log(leaderboard);
-			// leaderboard = leaderboard.map(async gameInfo => {
-			// 	console.log(gameInfo);
-            //     return leaderboard;
-			// });
-			// Promise.all(leaderboard).then(leaderboard => setLeaderboard(leaderboard));
-			setLeaderboard(leaderboard);
-		},
-        (err) => {
-            console.error(err)
-        }
+  useEffect(() => {
+    invoke("get_leaderboard_data", { gameTitle: game.title }).then(
+      (leaderboard) => {
+        console.log(leaderboard);
+        // leaderboard = leaderboard.map(async gameInfo => {
+        // 	console.log(gameInfo);
+        //     return leaderboard;
+        // });
+        // Promise.all(leaderboard).then(leaderboard => setLeaderboard(leaderboard));
+        setLeaderboard(leaderboard);
+      },
+      (err) => {
+        console.error(err);
+      }
     );
-	}, []);
+  }, []);
 
-	const getTopFiveLeaderboardEntries = (entries) => {
-		return [...entries]
-			.sort((a, b) => b.value_num - a.value_num)
-			.slice(0, 5);
-	};
+  const getTopFiveLeaderboardEntries = (entries) => {
+    return [...entries].sort((a, b) => b.value_num - a.value_num).slice(0, 5);
+  };
 
-	const startGame = async (gameId) => {
-		try {
-			await invoke("play_game", { id: gameId });
-			console.log(`Started Game ${game.title}`);
-		} catch (error) {
-			console.error(error);
-		}
-	};
+  const startGame = async (gameId) => {
+    try {
+      await invoke("play_game", { id: gameId });
+      console.log(`Started Game ${game.title}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-	const handlePlayGame = () => {
-		if (!isPlaceholder)
-			startGame(game.id);
+  const handlePlayGame = () => {
+    if (!isPlaceholder) startGame(game.id);
 
-		closeModal();
-		setTimeout(() => {
-			updatePage("home page");
-		}, 0);
-	};
-	
-	return (
-		<Modal
-			isOpen={showModal}
-			onClose={closeModal}
-			variant={"gameInfo"}
-			overlay
-			confirmLabel={!isPlaceholder ? "Play" : "Close"}
-			confirmLabelColorPrimary
-			title={game.title}
-			gameImageSrc={hasCoverImage ? game.coverImage : placeholder}
-			onConfirmation={handlePlayGame}
-			dataId="game-info"
-		>
-			<div className="game-info">
+    closeModal();
+    setTimeout(() => {
+      updatePage("home page");
+    }, 0);
+  };
+  let leaderboardData;
 
-				<div className="game-author">
-					<Text fontSize={"medium"}>{!isPlaceholder ? `Created by ${game.author}` : game.author}</Text>
-					{game.release_date && <Text fontSize="small">{formatDate(game.release_date)}</Text>}
-				</div>
+  return (
+    <Modal
+      isOpen={showModal}
+      onClose={closeModal}
+      variant={"gameInfo"}
+      overlay
+      confirmLabel={!isPlaceholder ? "Play" : "Close"}
+      confirmLabelColorPrimary
+      title={game.title}
+      gameImageSrc={hasCoverImage ? game.coverImage : placeholder}
+      onConfirmation={handlePlayGame}
+      dataId="game-info"
+    >
+      <div className="game-info">
+        <div className="game-author">
+          <Text fontSize={"medium"}>
+            {!isPlaceholder ? `Created by ${game.author}` : game.author}
+          </Text>
+          {game.release_date && (
+            <Text fontSize="small">{formatDate(game.release_date)}</Text>
+          )}
+        </div>
 
-				<Tabs dataId="game-tabs">
-					<Tab label={!isPlaceholder ? "Game Details" : "Details"}>
-						<div className="game-details">
-							<div className="game-description">
-								<Text fontSize="small">Description</Text>
-								<Text>
-									{game.summary.length > 250 ?
-										game.summary.slice(0, 250) + "..."
-										: game.summary
-									}
-								</Text>
-							</div>
-							<div className="game-genres">
-								
-								{game.genres && 
-									<>
-										<Text fontSize="small">Tags</Text>
-										<div className="game-tags">
-											
-											{game.multiplayer !== null && (
-												game.multiplayer === 'Multiplayer' &&
-													<Pill>Multiplayer</Pill>
-													|| <Pill>Single Player</Pill>
-											)}
+        <Tabs dataId="game-tabs">
+          <Tab label={!isPlaceholder ? "Game Details" : "Details"}>
+            <div className="game-details">
+              <div className="game-description">
+                <Text fontSize="small">Description</Text>
+                <Text>
+                  {game.summary.length > 250
+                    ? game.summary.slice(0, 250) + "..."
+                    : game.summary}
+                </Text>
+              </div>
+              <div className="game-genres">
+                {game.genres && (
+                  <>
+                    <Text fontSize="small">Tags</Text>
+                    <div className="game-tags">
+                      {game.multiplayer !== null &&
+                        ((game.multiplayer === "Multiplayer" && (
+                          <Pill>Multiplayer</Pill>
+                        )) || <Pill>Single Player</Pill>)}
 
-											{game.genres.map((genre, index) => {
-												return (
-													<Pill
-														key={index}
-														variant="secondary"
-													>
-														{genre}
-													</Pill>
-												);
-											})}
-										</div>
-									</>
-								}
-							</div>
-						</div>
-					</Tab>
-					{!isPlaceholder && 
-						<Tab label="Leaderboard">
-							{/* {leaderboard.length != 0 ?
-								<div className="game-leaderboard">
-									<Table headers={["Date", "Player", "Points"]}>
-										{getTopFiveLeaderboard().map((data, index) => (
-											<TableRow>
-												<TableData>{data.date}</TableData>
-												<TableData>{data.player}</TableData>
-												<TableData>{data.points}</TableData>
-												</TableRow>
-										))}
-									</Table>
-								</div>
-								:
-								<div className="game-leaderboard">
-									No leaderboard data t
-							 console.log(leaderboard);o show
-								</div>
-							 */ }
-							 {
-							//  leaderboard?.data ?
-							//  <Tabs dataId="leaderboard-tabs">
-							// 	{console.log(leaderboard)}
-							// 	{Object.entries(leaderboard.data).map(([value_name, entries]) => {
-							// 		<Tab label={value_name}>
-							// 			<Table headers={["Username", value_name, "Date"]}>
-							// 				{getTopFiveLeaderboardEntries(entries).map(({username, value_num, time_stamp}, index) => { console.log(username, value_num, time_stamp, index);
-							// 					<TableRow>
-							// 						<TableData>{username}</TableData>
-							// 						<TableData>{value_num}</TableData>
-							// 						<TableData>{time_stamp}</TableData>
-							// 					</TableRow>
-							// 				})}
-							// 			</Table>
-							// 		</Tab>
-							// 	})}
-							// </Tabs> :
-								// <div className="game-leaderboard">
-								// 	No leaderboard data to show
-								// </div>
-							
-							}
-							<Tabs dataId="leaderboard-tabs">
-								{Object.keys(leaderboard?.data ?? {}).map((value_name) => {
-							const leaderboardTitle = value_name.split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" ");
-							return (<Tab label={leaderboardTitle}>
-								<Table headers={["Username", leaderboardTitle, "Date"]}>
-									{getTopFiveLeaderboardEntries(leaderboard.data[value_name]).map(({username, value_num, time_stamp}, index) => {
-										return (
-											<TableRow key={index}>
-												<TableData>{username}</TableData>
-												<TableData>{value_num}</TableData>
-												<TableData>{time_stamp}</TableData>
-											</TableRow>
-										);
-									}
-									)}
-								</Table>
-							</Tab>
-							)
-								})}
-							</Tabs>	
-						</Tab>
-					}
-				</Tabs>
-			</div>
-
-		</Modal>
-    );
-};
+                      {game.genres.map((genre, index) => {
+                        return (
+                          <Pill key={index} variant="secondary">
+                            {genre}
+                          </Pill>
+                        );
+                      })}
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          </Tab>
+          {!isPlaceholder && (
+            <Tab label="Leaderboard">
+              {(leaderboardData = Object.keys(leaderboard?.data ?? {}))
+                .length != 0 ? (
+                <Tabs dataId="leaderboard-tabs">
+                  {leaderboardData.map((value_name) => {
+                    const leaderboardTitle = value_name
+                      .split(" ")
+                      .map(
+                        (word) => word.charAt(0).toUpperCase() + word.slice(1)
+                      )
+                      .join(" ");
+                    return (
+                      <Tab label={leaderboardTitle}>
+                        <Table headers={["Username", leaderboardTitle, "Date"]}>
+                          {getTopFiveLeaderboardEntries(
+                            leaderboard.data[value_name]
+                          ).map(
+                            ({ username, value_num, time_stamp }, index) => {
+                              return (
+                                <TableRow key={index}>
+                                  <TableData>{username}</TableData>
+                                  <TableData>{value_num}</TableData>
+                                  <TableData>{time_stamp}</TableData>
+                                </TableRow>
+                              );
+                            }
+                          )}
+                        </Table>
+                      </Tab>
+                    );
+                  })}
+                </Tabs>
+              ) : (
+                <div className="game-leaderboard">
+                  No leaderboard data to show
+                </div>
+              )}
+            </Tab>
+          )}
+        </Tabs>
+      </div>
+    </Modal>
+  );
+}
