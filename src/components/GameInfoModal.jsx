0620cb171c6a17e227762c0648e91353fdae12a4
saoski <saoski88@gmail.com>
2025-04-26 01:32:54 -0400

    Revert "Add checkbox"
    This reverts commit 9afd1cf7623a1df268469c7c40cc1f0adadcf5c7.

--- src/components/GameInfoModal.jsx
+++ src/components/GameInfoModal.jsx
@@ -9,7 +9,6 @@ import {
   TableRow,
   Tabs,
   Text,
-  Checkbox,
 } from "quackbox-design-system";
 import "../styles/GameInfoModal.css";
 import { invoke } from "@tauri-apps/api/tauri";
@@ -22,7 +21,6 @@ export default function GameInfoModal({ showModal, closeModal, game }) {
 
   const { updatePage } = usePageContext();
   const [leaderboard, setLeaderboard] = useState([]);
-  const [isAscending, setIsAscending] = useState(false);
   const isPlaceholder = game.title.toLowerCase().includes("coming soon");
   const hasCoverImage = !game.coverImage?.includes("null");
 
@@ -56,11 +54,7 @@ export default function GameInfoModal({ showModal, closeModal, game }) {
   }, []);
 
   const getTopFiveLeaderboardEntries = (entries) => {
-    return [...entries]
-      .sort((a, b) =>
-        isAscending ? a.value_num - b.value_num : b.value_num - a.value_num
-      )
-      .slice(0, 5);
+    return [...entries].sort((a, b) => b.value_num - a.value_num).slice(0, 5);
   };
 
   const startGame = async (gameId) => {
@@ -80,11 +74,6 @@ export default function GameInfoModal({ showModal, closeModal, game }) {
       updatePage("home page");
     }, 0);
   };
-
-  const handleAscendingChange = () => {
-    setIsAscending(!isAscending);
-  };
-
   let leaderboardData;
 
   return (
@@ -148,54 +137,35 @@ export default function GameInfoModal({ showModal, closeModal, game }) {
             <Tab label="Leaderboard">
               {(leaderboardData = Object.keys(leaderboard?.data ?? {}))
                 .length != 0 ? (
-                <div className="leaderboard-flex-parent">
-                  <div className="leaderboard-flex-checkbox">
-                    <Checkbox
-                      dataId="leaderboard-toggle-ascending"
-                      checked={isAscending}
-                      onChange={handleAscendingChange}
-                    ></Checkbox>
-                    <div style={{ textAlign: "center" }}>
-                      <Text fontSize="small">{isAscending ? "v" : "^"}</Text>
-                    </div>
-                  </div>
-                  <Tabs dataId="leaderboard-tabs">
-                    {leaderboardData.map((value_name) => {
-                      const leaderboardTitle = value_name
-                        .split(" ")
-                        .map(
-                          (word) => word.charAt(0).toUpperCase() + word.slice(1)
-                        )
-                        .join(" ");
-                      return (
-                        <Tab label={leaderboardTitle}>
-                          <div className="leaderboard-flex-table">
-                            <Table
-                              headers={["Username", leaderboardTitle, "Date"]}
-                            >
-                              {getTopFiveLeaderboardEntries(
-                                leaderboard.data[value_name]
-                              ).map(
-                                (
-                                  { username, value_num, time_stamp },
-                                  index
-                                ) => {
-                                  return (
-                                    <TableRow key={index}>
-                                      <TableData>{username}</TableData>
-                                      <TableData>{value_num}</TableData>
-                                      <TableData>{time_stamp}</TableData>
-                                    </TableRow>
-                                  );
-                                }
-                              )}
-                            </Table>
-                          </div>
-                        </Tab>
-                      );
-                    })}
-                  </Tabs>
-                </div>
+                <Tabs dataId="leaderboard-tabs">
+                  {leaderboardData.map((value_name) => {
+                    const leaderboardTitle = value_name
+                      .split(" ")
+                      .map(
+                        (word) => word.charAt(0).toUpperCase() + word.slice(1)
+                      )
+                      .join(" ");
+                    return (
+                      <Tab label={leaderboardTitle}>
+                        <Table headers={["Username", leaderboardTitle, "Date"]}>
+                          {getTopFiveLeaderboardEntries(
+                            leaderboard.data[value_name]
+                          ).map(
+                            ({ username, value_num, time_stamp }, index) => {
+                              return (
+                                <TableRow key={index}>
+                                  <TableData>{username}</TableData>
+                                  <TableData>{value_num}</TableData>
+                                  <TableData>{time_stamp}</TableData>
+                                </TableRow>
+                              );
+                            }
+                          )}
+                        </Table>
+                      </Tab>
+                    );
+                  })}
+                </Tabs>
               ) : (
                 <div className="game-leaderboard">
                   No leaderboard data to show
