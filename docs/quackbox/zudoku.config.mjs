0e822d49b8b64df8dec1d7e3440cec9c13e9c426
saoski <saoski88@gmail.com>
2025-05-16 13:28:21 -0400

    Created Zudoku app

---
+++ docs/quackbox/zudoku.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('zudoku').ZudokuConfig} */
+const config = {
+  topNavigation: [
+    { id: "docs", label: "Documentation" },
+    { id: "api", label: "API Reference" },
+  ],
+  sidebar: {
+    docs: [
+      {
+        type: "category",
+        label: "Overview",
+        items: ["docs/introduction", "docs/example"],
+      },
+    ],
+  },
+  redirects: [{ from: "/", to: "/docs/introduction" }],
+  apis: {
+    type: "file",
+    input: "./apis/openapi.yaml",
+    navigationId: "api",
+  },
+  docs: {
+    files: "/pages/**/*.{md,mdx}",
+  },
+};
+
+export default config;
