19fefc123342ee73dc7aa865573704f3e66706a6
saoski <saoski88@gmail.com>
2025-10-26 14:25:12 -0400

    Add V2 get_save_data

--- src-tauri/src/game_dev_api/v2_handlers.rs
+++ src-tauri/src/game_dev_api/v2_handlers.rs
@@ -3,7 +3,7 @@ use axum::{
     http::StatusCode,
     response::IntoResponse, Json,
 };
-use serde_json::json;
+use serde_json::{json, Value};
 
 use crate::db::{self, models::Save};
 
@@ -84,4 +84,45 @@ pub async fn upsert_save_data(
         "time_stamp":new_save.time_stamp,
         "file_name": payload.file_name,
     }))
+}
+
+pub async fn get_save_data(
+    State(state): State<ApiState>,
+    State(game_state): State<GameStateShared>,
+    Path(player_slot): Path<i16>,
+    params: Query<SaveDataGetParams>
+) -> impl IntoResponse {
+    println!("Getting save data!");
+    let game_id = game_state.id.read().await.unwrap().to_string();
+    drop(game_state);
+
+    let user_id_s = player_slot.to_string();
+
+    let save_data_entries = db::get_save_data(
+        &Some(game_id),
+        &Some(user_id_s),
+        &None,
+        &params.regex,
+        None,
+        None,
+        None,
+        &state.database_path,
+    )
+    .await;
+
+    match save_data_entries {
+        Ok(save_data) => {
+            let mut json_response = Vec::new();
+
+            for entry in save_data {
+                json_response.push(serde_json::json!({
+                            "data":serde_json::from_slice::<Value>(&entry.data).expect("Failed to deserialize BSON data"),
+                            "file_name": entry.file_name,
+                            "time_stamp": entry.time_stamp
+                        }));
+            }
+            return Json(json_response).into_response();
+        }
+        Err(e) => (StatusCode::BAD_REQUEST, e.to_string()).into_response(),
+    }
 }
