59729b2d11604faae5d70bec165d9883e42aaf11
saoski <saoski88@gmail.com>
2025-10-26 14:16:32 -0400

    Add v2 upsert save data

--- src-tauri/src/game_dev_api/v2_handlers.rs
+++ src-tauri/src/game_dev_api/v2_handlers.rs
@@ -5,22 +5,22 @@ use axum::{
 };
 use serde_json::json;
 
-use crate::db;
+use crate::db::{self, models::Save};
 
 use super::{ApiState, GameStateShared};
 
-pub struct SaveDataGetParamsV2 {
+pub struct SaveDataGetParams {
     regex: Option<String>,
     limit: Option<i64>,
     offset: Option<i64>,
     ascending: Option<bool>,
 }
 
-pub async fn get_save_data_info_v2(
+pub async fn get_save_data_info(
     State(state): State<ApiState>,
     State(game_state): State<GameStateShared>,
     Path(player_slot): Path<i16>,
-    params: Query<SaveDataGetParamsV2>,
+    params: Query<SaveDataGetParams>,
 ) -> impl IntoResponse {
     let game_id = game_state.id.read().await.unwrap().to_string();
     drop(game_state);
@@ -53,4 +53,35 @@ pub async fn get_save_data_info_v2(
         }
         Err(e) => (StatusCode::BAD_REQUEST, e.to_string()).into_response(),
     }
+}
+
+pub struct SaveDataPost {
+    data: serde_json::Value,
+    file_name: String
+}
+
+pub async fn upsert_save_data(
+    State(state): State<ApiState>,
+    State(game_state): State<GameStateShared>,
+    Path(player_slot): Path<i16>,
+    Json(payload): Json<SaveDataPost>
+) -> impl IntoResponse {
+    let game_id = game_state.id.read().await.unwrap().to_string();
+    drop(game_state);
+    let user_id = player_slot.to_string();
+
+    let new_save: Save = db::set_save(
+        &user_id,
+        &game_id,
+        payload.file_name.as_str(),
+        &serde_json::to_vec(&payload.data).unwrap(),
+        &state.database_path,
+    )
+    .await;
+
+    Json(serde_json::json!({
+        "data": payload.data,
+        "time_stamp":new_save.time_stamp,
+        "file_name": payload.file_name,
+    }))
 }
