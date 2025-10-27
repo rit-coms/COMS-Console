424cffe3e849d636dca91ef618328c19f8dba64e
saoski <saoski88@gmail.com>
2025-10-26 22:02:49 -0400

    Complete rest of handlers (excluding playerslots)

--- src-tauri/src/game_dev_api/v2_handlers.rs
+++ src-tauri/src/game_dev_api/v2_handlers.rs
@@ -6,7 +6,7 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 
-use crate::{db::{self, models::Save, }, frontend_api::GameSenderState, game_dev_api::GameState};
+use crate::{db::{self, models::Save, schema::leaderboard::value_name, }, frontend_api::GameSenderState, game_dev_api::GameState};
 
 use super::{ApiState, GameStateShared};
 
@@ -129,3 +129,143 @@ pub async fn get_save_data(
         Err(e) => (StatusCode::BAD_REQUEST, e.to_string()).into_response(),
     }
 }
+
+#[derive(Deserialize, Serialize)]
+pub struct LeaderboardPost {
+    pub value_num: f64
+}
+
+pub async fn insert_leaderboard_entry(
+    State(state): State<ApiState>,
+    State(game_state): State<GameStateShared>,
+    Path((player_slot, leaderboard_name)): Path<(i16, String)>,
+    Json(payload): Json<LeaderboardPost>
+) -> impl IntoResponse {
+    let game_id = game_state.id.read().await.unwrap().to_string(); drop(game_state);
+    let user_id = player_slot.to_string();
+
+    db::insert_leaderboard_entry(
+        &user_id,
+        &game_id,
+        &leaderboard_name,
+        payload.value_num,
+        &state.database_path,
+    )
+    .expect("Faied to enter leaderboard entry");
+
+    Json(serde_json::json!({
+        "leaderboard_name":leaderboard_name,
+        "value_num":payload.value_num,
+        "user_id":user_id,
+    }))
+}
+
+pub struct LeaderboardGetParams {
+    limit: Option<i64>,
+    offset: Option<i64>,
+    ascending: Option<bool>,
+}
+
+pub async fn get_leaderboard_global(
+    State(state): State<ApiState>,
+    State(game_state): State<GameStateShared>,
+    Path(leaderboard_name): Path<String>,
+    params: Query<LeaderboardGetParams>
+) -> impl IntoResponse {
+    let game_id = game_state.id.read().await.unwrap().to_string();
+    drop(game_state);
+
+    let leaderboard_entries = db::get_leaderboard(
+        Some(game_id),
+        None,
+        params.limit,
+        params.ascending,
+        Some(leaderboard_name.clone()),
+        params.offset,
+        &state.database_path,
+    )
+    .await;
+
+    let mut json_response: Vec<serde_json::Value> = Vec::new();
+
+    for entry in leaderboard_entries {
+        json_response.push(serde_json::json!({
+            "leaderboard_name": entry.value_name,
+            "value_num": entry.value_num,
+            "user_id": str::parse::<i16>(&entry.user_id).unwrap(),
+            "time_stamp": entry.time_stamp
+        }));
+    }
+
+    Json(json_response).into_response()
+}
+
+pub async fn get_leaderboard_user(
+    State(state): State<ApiState>,
+    State(game_state): State<GameStateShared>,
+    Path((user_id, leaderboard_name)): Path<(String, String)>,
+    params: Query<LeaderboardGetParams>
+) -> impl IntoResponse {
+    let game_id = game_state.id.read().await.unwrap().to_string();
+    drop(game_state);
+
+    let leaderboard_entries = db::get_leaderboard(
+        Some(game_id),
+        Some(user_id),
+        params.limit,
+        params.ascending,
+        Some(leaderboard_name.clone()),
+        params.offset,
+        &state.database_path,
+    )
+    .await;
+
+    let mut json_response: Vec<serde_json::Value> = Vec::new();
+
+    for entry in leaderboard_entries {
+        json_response.push(serde_json::json!({
+            "value_name": entry.value_name,
+            "value_num": entry.value_num,
+            "player_slot": str::parse::<i16>(&entry.user_id).unwrap(),
+            "time_stamp": entry.time_stamp
+        }));
+    }
+
+    Json(json_response).into_response()
+}
+
+pub async fn get_leaderboard_player_slot(
+    State(state): State<ApiState>,
+    State(game_state): State<GameStateShared>,
+    Path((player_slot, leaderboard_name)): Path<(String, String)>,
+    params: Query<LeaderboardGetParams>
+) -> impl IntoResponse {
+    let game_id = game_state.id.read().await.unwrap().to_string();
+    drop(game_state);
+    let user_id = player_slot.to_string();
+
+    let leaderboard_entries = db::get_leaderboard(
+        Some(game_id),
+        Some(user_id),
+        params.limit,
+        params.ascending,
+        Some(leaderboard_name.clone()),
+        params.offset,
+        &state.database_path,
+    )
+    .await;
+
+    let mut json_response: Vec<serde_json::Value> = Vec::new();
+
+    for entry in leaderboard_entries {
+        json_response.push(serde_json::json!({
+            "value_name": entry.value_name,
+            "value_num": entry.value_num,
+            "player_slot": str::parse::<i16>(&entry.user_id).unwrap(),
+            "time_stamp": entry.time_stamp
+        }));
+    }
+
+    Json(json_response).into_response()
+}
+
